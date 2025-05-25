4b1736f68e8f6d2ad524ee59292d3b8613180d59
Anurag's App <anuragsingh@gmail.com>
2025-05-25 14:12:14 +0530

    new-questions

---
+++ UniqueQuestion.cpp/Pythogoreas.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include<unordered_set>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+public:
+    bool pythagoreanTriplet(vector<int> &arr)
+    {
+        // code here
+        unordered_set<int> st(arr.begin(), arr.end());
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int a = arr[i];
+                int b = arr[j];
+                int sq = (a * a) + (b * b);
+                int c = sqrt(sq);
+                if (c * c == sq && st.find(c) != st.end())
+                    return 1;
+            }
+        }
+        return 0;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
