95171dfc20c4b41ef51ae9fbd7f1462424b1b68c
Anurag's App <anuragsingh@gmail.com>
2025-07-28 15:50:35 +0530

    new-questions

---
+++ RandomQuestion/power.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main(){
+      
+  
+  int a = 15;
+  int b = 2;
+  int ans = 1; 
+  
+  for(int i=1; i<=b; i++){
+    ans = ans * a; 
+  }
+  cout << ans << endl;
+   
+  
+  return 0;
+}
