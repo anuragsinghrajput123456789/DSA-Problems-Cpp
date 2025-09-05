f3326b3423a99ad609f1292ba31359400ec1993e
Anurag's App <anuragsingh@gmail.com>
2025-09-05 14:10:52 +0530

    interview-rev

--- RandomQuestion/playingWithpointers.cpp
+++ RandomQuestion/playingWithpointers.cpp
@@ -5,60 +5,60 @@
 using namespace std; 
 int main(){
   
-// int arr[10] = {1,2,3};
-// int n = 10;
-// int *p = arr; 
+int arr1[10] = {1,2,3};
+int n = 10;
+int *p1 = arr1; 
  
-// cout << "address of the first memory block is :" << arr << endl; 
-// cout << &arr[0] << endl; 
-// cout << " value at 0th index is " << *arr << endl; 
-// cout << *arr + 1 << endl; 
-// cout << *(arr+1)<< endl;
-// cout << (*arr) + 1 << endl;
- 
- 
-// for(auto i=0; i<n; i++){
-//   cout << *(arr+i) << endl; 
-// }
- 
-// int i = 3; 
- 
-// cout << i[arr] << endl;
- 
-// int temp[10]= {1,2}; 
- 
-// cout << sizeof(temp) << endl;
-// int *ptr = &temp[0];
-// cout << sizeof(ptr) << endl;
-// cout << sizeof(*ptr) << endl;
-// cout << sizeof(&ptr) << endl; 
+cout << "address of the first memory block is :" << arr1 << endl; 
+cout << &arr1[0] << endl; 
+cout << " value at 0th index is " << *arr1 << endl; 
+cout << *arr1 + 1 << endl; 
+cout << *(arr1+1)<< endl;
+cout << (*arr1) + 1 << endl;
  
  
-// int a[20] = {1,2,3,4,6,8};
-// int *p = &a[0]; 
-// cout << "->" << &p << endl; 
-// cout << &a[0] << endl; 
-// cout << &a << endl; 
-// cout << a << endl; 
+for(auto i=0; i<n; i++){
+  cout << *(arr1+i) << endl; 
+}
+ 
+int i = 3; 
+ 
+cout << i[arr1] << endl;
+ 
+int temp[10]= {1,2}; 
+ 
+cout << sizeof(temp) << endl;
+int *ptr = &temp[0];
+cout << sizeof(ptr) << endl;
+cout << sizeof(*ptr) << endl;
+cout << sizeof(&ptr) << endl; 
  
  
-// int arr[10]; 
-// // arr = arr + 1; not possible
+int a[20] = {1,2,3,4,6,8};
+int *p2 = &a[0]; 
+cout << "->" << &p2 << endl; 
+cout << &a[0] << endl; 
+cout << &a << endl; 
+cout << a << endl; 
+ 
+ 
+int arr2[10]; 
+// arr = arr + 1; not possible
 
 
-//   int *ptr  = &arr[0];
-//   cout << ptr << endl; 
-//   ptr = ptr + 1; 
-//   cout << ptr << endl;
+  int *ptr2  = &arr2[0];
+  cout << ptr2 << endl; 
+  ptr2 = ptr2 + 1; 
+  cout << ptr2 << endl;
  
  
  
  
  
- int arr[5] = {1,2,4,5};
+ int arr3[5] = {1,2,4,5};
  char ch[6] = "abcdf";
  
- cout << arr << endl; 
+ cout << arr3 << endl; 
  cout << ch << endl;
  
  
@@ -73,9 +73,9 @@ int main(){
  
  
  char temp = 'z'; 
- char *p  = &temp;
+ char *cp  = &temp;
  
- cout << p << endl; 
+ cout << static_cast<void*>(cp) << endl; 
  
  
  
