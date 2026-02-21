42caa038a7cd8450dd313b598e47b7c73f43a451
Anurag's App <anuragsingh@gmail.com>
2026-02-21 17:10:31 +0530

    strings_questions

---
+++ GeekforGeeks/leetCode-1047.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string temp = "";
+        int i = 0;
+        
+        while (i < s.length()) {
+            if (temp.empty() || s[i] != temp.back()) {
+                temp.push_back(s[i]);
+            } else {
+                temp.pop_back();
+            }
+            i++;
+        }
+        
+        return temp;
+    }
+};
+
+int main(){
+
+
+
+    return 0; 
+}
